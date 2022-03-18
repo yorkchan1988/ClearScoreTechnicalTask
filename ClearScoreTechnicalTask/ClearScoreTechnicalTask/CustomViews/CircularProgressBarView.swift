bf90161f8e8c62c946352a6d6f655226d7ecd04d
Chan, York T <york.t.chan@accenture.com>
2022-03-18 17:51:28 +0000

    add background to the app and set up the circular progress view

---
+++ ClearScoreTechnicalTask/ClearScoreTechnicalTask/CustomViews/CircularProgressBarView.swift
@@ -0,0 +1,71 @@
+//
+//  CircularProgressBarView.swift
+//  ClearScoreTechnicalTask
+//
+//  Created by YORK CHAN on 18/3/2022.
+//
+
+import Foundation
+import UIKit
+
+class CircularProgressBarView: UIView {
+    
+    private var circleLayer = CAShapeLayer()
+    private var progressLayer = CAShapeLayer()
+    private var startPoint = CGFloat(-Double.pi / 2)
+    private var endPoint = CGFloat(3 * Double.pi / 2)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        createCircularPath()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.backgroundColor = .clear
+    }
+    
+    func createCircularPath() {
+        // created circularPath for circleLayer and progressLayer
+        let circularPath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0), radius: frame.size.width / 2.0, startAngle: startPoint, endAngle: endPoint, clockwise: true)
+        // circleLayer path defined to circularPath
+        circleLayer.path = circularPath.cgPath
+        // ui edits
+        circleLayer.fillColor = UIColor.clear.cgColor
+        circleLayer.lineCap = .round
+        circleLayer.lineWidth = 20.0
+        circleLayer.strokeEnd = 1.0
+        circleLayer.strokeColor = UIColor.white.cgColor
+        // added circleLayer to layer
+        layer.addSublayer(circleLayer)
+        // progressLayer path defined to circularPath
+        progressLayer.path = circularPath.cgPath
+        // ui edits
+        progressLayer.fillColor = UIColor.clear.cgColor
+        progressLayer.lineCap = .round
+        progressLayer.lineWidth = 10.0
+        progressLayer.strokeEnd = 0
+        progressLayer.strokeColor = UIColor.green.cgColor
+        // added progressLayer to layer
+        layer.addSublayer(progressLayer)
+    }
+    
+    func progressAnimation(duration: TimeInterval) {
+        // created circularProgressAnimation with keyPath
+        let circularProgressAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        // set the end time
+        circularProgressAnimation.duration = duration
+        circularProgressAnimation.toValue = 1.0
+        circularProgressAnimation.fillMode = .forwards
+        circularProgressAnimation.isRemovedOnCompletion = false
+        progressLayer.add(circularProgressAnimation, forKey: "progressAnim")
+    }
+}
--- ClearScoreTechnicalTask/ClearScoreTechnicalTask/Scenes/Dashboard/DashboardViewController.swift
+++ ClearScoreTechnicalTask/ClearScoreTechnicalTask/Scenes/Dashboard/DashboardViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class DashboardViewController: UIViewController {
     
+    private let circularViewDuration: TimeInterval = 2
+    
+    @IBOutlet weak var circularProgressBarView: CircularProgressBarView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "Dashboard"
+        
+        setUpCircularProgressBarView()
+    }
+    
+    func setUpCircularProgressBarView() {
+        // call the animation with circularViewDuration
+        circularProgressBarView.progressAnimation(duration: circularViewDuration)
     }
 }
