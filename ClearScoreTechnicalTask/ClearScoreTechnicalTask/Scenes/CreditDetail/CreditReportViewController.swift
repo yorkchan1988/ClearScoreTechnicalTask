7c822bf6acfee4f567d7d6da78380124230a6143
Chan, York T <york.t.chan@accenture.com>
2022-03-19 17:22:09 +0000

    finish detail view

--- ClearScoreTechnicalTask/ClearScoreTechnicalTask/Scenes/CreditDetail/CreditReportViewController.swift
+++ ClearScoreTechnicalTask/ClearScoreTechnicalTask/Scenes/CreditDetail/CreditReportViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CreditReportViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     var viewModel: CreditReportViewModel!
     var router: CreditReportRouter!
     
@@ -17,5 +19,26 @@ class CreditReportViewController: UIViewController {
         super.viewDidLoad()
         
         navigationItem.title = "Credit Report"
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+    }
+}
+
+extension CreditReportViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let row = indexPath.row
+        let cell: UITableViewCell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = viewModel.getCellTitleByIndex(index: row)
+        cell.textLabel?.textColor = .white
+        let value = viewModel.getCellValueByIndex(index: row)
+        cell.detailTextLabel?.text = value
+        cell.detailTextLabel?.textColor = .white
+        return cell
     }
 }
--- ClearScoreTechnicalTask/ClearScoreTechnicalTask/Scenes/CreditDetail/CreditReportViewModel.swift
+++ ClearScoreTechnicalTask/ClearScoreTechnicalTask/Scenes/CreditDetail/CreditReportViewModel.swift
@@ -14,4 +14,46 @@ class CreditReportViewModel: ViewModel {
     init(creditValue: CreditValue) {
         self.creditValue = creditValue
     }
+    
+    func getCellTitleByIndex(index: Int) -> String? {
+        switch(index) {
+        case 0:
+            return "Client Ref"
+        case 1:
+            return "Current Short Term Debt"
+        case 2:
+            return "Current Short Term Credit Limit"
+        case 3:
+            return "Current Long Term Debt"
+        case 4:
+            return "Current Long Term Credit Limit"
+        case 5:
+            return "Equifax Score Band"
+        case 6:
+            return "Equifax Score Band Description"
+        default:
+            return nil
+        }
+    }
+    
+    func getCellValueByIndex(index: Int) -> String? {
+        switch(index) {
+        case 0:
+            return self.creditValue.creditReportInfo.clientRef
+        case 1:
+            return String(format: "%ld", self.creditValue.creditReportInfo.currentShortTermDebt ?? 0)
+        case 2:
+            return String(format: "%ld", self.creditValue.creditReportInfo.currentShortTermCreditLimit ?? 0)
+        case 3:
+            return String(format: "%ld", self.creditValue.creditReportInfo.currentLongTermDebt ?? 0)
+        case 4:
+            return String(format: "%ld", self.creditValue.creditReportInfo.currentLongTermCreditLimit ?? 0)
+        case 5:
+            return String(format: "%ld", self.creditValue.creditReportInfo.equifaxScoreBand)
+        case 6:
+            return self.creditValue.creditReportInfo.equifaxScoreBandDescription
+        default:
+            return nil
+        }
+    }
 }
